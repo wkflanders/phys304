656b52b9c2cd77aba8f46ab6773c3efe7a24eec0
wkflanders <wflanders@haverford.edu>
2023-05-12 04:07:15 -0400

    final proj commit

--- final_project/PCA.py
+++ final_project/PCA.py
@@ -3,62 +3,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
+# Fetching data
 spectra = retrieve_sdss_spectra()[0]
 wavelength = retrieve_sdss_spectra()[1]
 
+# Plotting spectra
 def spectra_plot(n):
     fig = plt.figure(figsize=(10, 8))
-    # fig.subplots_adjust(left=0.05, right=0.95, wspace=0.05,
-    #                     bottom=0.1, top=0.95, hspace=0.05)
-
     n_plots = n
 
     for i in range(n_plots):
         ax = fig.add_subplot(n_plots, 1, i+1)
         ax.plot(wavelength, spectra[i])
-        ax.set_ylabel("Flux")
-    ax.set_xlabel("Wavelength")
+        ax.set_ylabel(r"Flux")
+    ax.set_xlabel(r"Wavelength")
     plt.show()
 
-def compute_evals(n, plot=True):
+# Calculating the principal components
+def compute_evals(n):
     spec_mean = spectra.mean(0)
     n_components = n
-
+    # Using sklearn PCA class to calculate components
     pca = PCA(n_components - 1, svd_solver='randomized')
     pca.fit(spectra)
     pca_comp=np.vstack([spec_mean,pca.components_])
     eigenvals = pca.explained_variance_ratio_
+    # Returning evals and evecs
+    return eigenvals, pca_comp
 
+# Plotting the principal components
+def plot_pca(pca_comp, n):
     fig = plt.figure(figsize=(12,10))
-    if plot is True:
-        for j in range(n_components):
-            ax = fig.add_subplot(n_components, 1, j+1)
-            if j == 0:
-                ax.set_title("Mean")
-                ax.plot(wavelength, pca_comp[j])
-                ax.set_ylabel("Flux")
-            elif j == 1:
-                ax.set_title(str(j) + "st Component")
-                ax.plot(wavelength, pca_comp[j])
-                ax.set_ylabel("Flux")
-            elif j == 2:
-                ax.set_title(str(j) + "nd Component")
-                ax.plot(wavelength, pca_comp[j])
-                ax.set_ylabel("Flux")
-            elif j == 3:
-                ax.set_title(str(j) + "rd Component")
-                ax.plot(wavelength, pca_comp[j])
-                ax.set_ylabel("Flux")
-            else:
-                ax.set_title(str(j) + "th Component")
-                ax.plot(wavelength, pca_comp[j])
-                ax.set_ylabel("Flux")
-        ax.set_xlabel("Wavelength")
-        plt.show()
-    return eigenvals
+    for j in range(n):
+        ax = fig.add_subplot(n, 1, j+1)
+        if j == 0:
+            ax.set_title(r"Mean")
+            ax.plot(wavelength, pca_comp[j])
+            ax.set_ylabel(r"Flux")
+        elif j == 1:
+            ax.set_title(str(j) + r"st Component")
+            ax.plot(wavelength, pca_comp[j])
+            ax.set_ylabel(r"Flux")
+        elif j == 2:
+            ax.set_title(str(j) + r"nd Component")
+            ax.plot(wavelength, pca_comp[j])
+            ax.set_ylabel(r"Flux")
+        elif j == 3:
+            ax.set_title(str(j) + r"rd Component")
+            ax.plot(wavelength, pca_comp[j])
+            ax.set_ylabel(r"Flux")
+        else:
+            ax.set_title(str(j) + r"th Component")
+            ax.plot(wavelength, pca_comp[j])
+            ax.set_ylabel(r"Flux")
+    ax.set_xlabel(r"Wavelength")
+    plt.show()
+
 
 if __name__ == "__main__":
+    evals = compute_evals(5)[0]
+    pca_comp = compute_evals(5)[1]
+    plot_pca(pca_comp, 5)
     spectra_plot(5)
-
-def reconstruct(n):
-    eigenvals = compute_evals(n, False)
--- final_project/SDSS_data.py
+++ final_project/SDSS_data.py
@@ -1,9 +1,11 @@
 from astroML.datasets import sdss_corrected_spectra
 
 def retrieve_sdss_spectra():
-
+    # Fetching data set form astroML
     data = sdss_corrected_spectra.fetch_sdss_corrected_spectra()
+    # Grabbing spectra from data
     spectra = sdss_corrected_spectra.reconstruct_spectra(data)
+    # Grabbing wavelengths from data
     wavelengths = sdss_corrected_spectra.compute_wavelengths(data)
 
     return spectra, wavelengths
